11898603918b603ee3ea1c462c8a06a2f5a9f4b9
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-17 20:31:38 +0200

    Initial Commit

---
+++ InstafilterInSwiftUI/InstafilterInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterInSwiftUIApp.swift
+//  InstafilterInSwiftUI
+//
+//  Created by test on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
