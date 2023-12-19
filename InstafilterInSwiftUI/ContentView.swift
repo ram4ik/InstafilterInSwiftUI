3f23ab1543c8c4fd3a9b755ec44c2ff98775c4c2
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-19 20:37:39 +0200

    content unavailable view

--- InstafilterInSwiftUI/ContentView.swift
+++ InstafilterInSwiftUI/ContentView.swift
@@ -8,39 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
-    
-    @State private var blurAmount = 0.0
-    
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-            
-            Slider(value: $blurAmount, in: 0...20)
-                .onChange(of: blurAmount) { oldValue, newValue in
-                    print("New value is \(newValue)")
-                }
-            
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
-            }
-            Button("Confirmation") {
-                showingConfirmation.toggle()
-            }
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
-        }
-        .padding()
+        ContentUnavailableView("No snippets", systemImage: "swift")
     }
 }
 
